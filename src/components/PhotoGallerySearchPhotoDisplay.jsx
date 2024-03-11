79661212ba5ed50197aae6df4e5a845f3588dd89
priyanka gupta <34priyankagupta@gmail.com>
2024-03-11 23:35:10 +0530

    adding hover effect on images

--- src/components/PhotoGallerySearchPhotoDisplay.jsx
+++ src/components/PhotoGallerySearchPhotoDisplay.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 export default function PhotoGallerySearchPhotoDisplay() {
   const { results } = useLoaderData();
+  const [showImgDetail, setShowImgDetail] = useState("");
   console.log("loadedGallery", results);
+  const showImageDetails = (image) => {
+    setShowImgDetail(image.id);
+    console.log("image", image);
+  };
   return (
     <div className="imageDisplayContainer">
-      {results.map(({ urls, alt_description, id, description }) => (
-        <div className="imageDisplay" key={id}>
-          <img className="imageDisplay" src={urls.small} alt={alt_description} />
-          <p className="description">{description}</p>
-        </div>
-      ))}
+      {results.map((image) => {
+        const { urls, alt_description, id } = image;
+        return (
+          <div className="imageGrid" key={id}>
+            <img
+              className="imageDisplay"
+              src={urls.small}
+              alt={alt_description}
+              onClick={() => showImageDetails(image)}
+              onMouseOver={() => showImageDetails(image)}
+            />
+            {showImgDetail === id && (
+              <div className="detailsContainer">hiii</div>
+            )}
+          </div>
+        );
+      })}
     </div>
   );
 }
--- src/services/PhotoGalleryUnplashService.js
+++ src/services/PhotoGalleryUnplashService.js
@@ -1,16 +1,16 @@
-import axios from "axios";
-// import getSearchData from "./getSearch";
+// import axios from "axios";
+import getSearchData from "./getSearch";
 
-const clientId = process.env.REACT_APP_API_KEY;
+// const clientId = process.env.REACT_APP_API_KEY;
 
-const UNSPLASH_ROOT = process.env.REACT_APP_API_URL;
+// const UNSPLASH_ROOT = process.env.REACT_APP_API_URL;
 
 // https://unsplash.com/documentation#search
 export const getPhotosByQuery = async (/*{ query }*/) => {
-    const query = "ocean";
-    const { data } = await axios.get(
-      `${UNSPLASH_ROOT}/search/photos?query=${query}&client_id=${clientId}&per_page=20`
-    );
-//   const data = new Promise((resolve) => resolve(getSearchData));
+    // const query = "ocean";
+    // const { data } = await axios.get(
+    //   `${UNSPLASH_ROOT}/search/photos?query=${query}&client_id=${clientId}&per_page=20`
+    // );
+  const data = new Promise((resolve) => resolve(getSearchData));
   return data;
 };
