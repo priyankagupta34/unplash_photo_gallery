6400b650f8efbaa4b70648c2ec9c3a865bf7262b
priyanka gupta <34priyankagupta@gmail.com>
2024-03-11 22:26:59 +0530

    removing contstant js file

--- src/services/PhotoGalleryUnplashService.js
+++ src/services/PhotoGalleryUnplashService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import getSearchData from "./getSearch";
+// import getSearchData from "./getSearch";
 
 const clientId = process.env.REACT_APP_API_KEY;
 
@@ -7,10 +7,10 @@ const UNSPLASH_ROOT = process.env.REACT_APP_API_URL;
 
 // https://unsplash.com/documentation#search
 export const getPhotosByQuery = async (/*{ query }*/) => {
-  //   const query = "ocean";
-  //   const { data } = await axios.get(
-  //     `${UNSPLASH_ROOT}/search/photos?query=${query}&client_id=${clientId}&per_page=20`
-  //   );
-  const data = new Promise((resolve) => resolve(getSearchData));
+    const query = "ocean";
+    const { data } = await axios.get(
+      `${UNSPLASH_ROOT}/search/photos?query=${query}&client_id=${clientId}&per_page=20`
+    );
+//   const data = new Promise((resolve) => resolve(getSearchData));
   return data;
 };
