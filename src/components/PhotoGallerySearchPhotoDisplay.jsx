3ba53aea9ffadc514286d9feab36f053ac23c4a2
priyanka gupta <34priyankagupta@gmail.com>
2024-03-12 09:49:03 +0530

    adding hover effect on images

--- src/components/PhotoGallerySearchPhotoDisplay.jsx
+++ src/components/PhotoGallerySearchPhotoDisplay.jsx
@@ -22,7 +22,7 @@ export default function PhotoGallerySearchPhotoDisplay() {
               onClick={() => showImageDetails(image)}
               onMouseOver={() => showImageDetails(image)}
             />
-            {showImgDetail === id && (
+            {showImgDetail && (
               <div className="detailsContainer">hiii</div>
             )}
           </div>
